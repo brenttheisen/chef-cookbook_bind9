055a3cab5bf0658ddafc85de8f781e872e82956a
Brent Theisen <brent@bantamlabs.com>
2014-05-19 11:30:37 -0500

    Added name and supports.

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             "bind9"
 maintainer       "Mike Adolphs"
 maintainer_email "mike@fooforge.com"
 license          "Apache 2.0"
@@ -5,6 +6,6 @@ description      "Installs/Configures bind9"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.9"
 
-%w{ ubuntu debian centos }.each do |os|
+%w{ ubuntu debian centos redhat fedora amazon }.each do |os|
   supports os
 end
